0ec7f9191eadcc3d37b0480a8a65826972873798
Zarha <zarhaselene@gmail.com>
2025-01-07 11:47:01 +0100

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes"; // Import Theme
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Theme>
+      <App />
+    </Theme>
+  </StrictMode>
+);
